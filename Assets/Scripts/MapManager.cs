a68a6427a651bb21e93233e7376ff24fb2247ac0
Yuval Ozeri <Yuuval@gmail.com>
2021-01-04 00:59:52 +0200

    First Commit

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map = default;
+    [SerializeField]
+    private Tilemap trees = default;
+    [SerializeField]
+    private Tilemap stones = default;
+
+    [SerializeField]
+    private ParticleSystem particles = default;
+
+    private Touch touch;
+    private PopupManager popupManager;
+
+    private float currCountdownValue;
+
+    private void Start()
+    {
+        popupManager = GetComponent<PopupManager>();
+    }
+
+
+    void Update()
+    {
+        #region computer
+        if (Input.GetMouseButtonDown(0) && !popupManager.displayingPopup)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            if (trees.GetTile(gridPosition) == null)
+            {
+                TileBase clickedTile = map.GetTile(gridPosition);
+                if (clickedTile != null)
+                {
+                    particles.transform.position = mousePosition;
+                    StartCoroutine(StartCountdown(gridPosition));
+                }
+            }
+            else
+            {
+                popupManager.DisplayPopup();
+            }
+            
+        }
+        #endregion
+
+        #region phone
+        if (Input.touchCount > 0 && !popupManager.displayingPopup)
+        {
+            touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            Vector3Int gridPosition = map.WorldToCell(touchPosition);
+
+            if (trees.GetTile(gridPosition) == null)
+            {
+                TileBase clickedTile = map.GetTile(gridPosition);
+                if (clickedTile != null)
+                {
+                    particles.transform.position = touchPosition;
+                    StartCoroutine(StartCountdown(gridPosition));
+                }
+            }
+            else
+            {
+                popupManager.DisplayPopup();
+            }
+
+        }
+        #endregion
+    }
+
+
+    public IEnumerator StartCountdown(Vector3Int gridPosition, float countdownValue = 2)
+    {
+
+        particles.Play();
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+
+        particles.Stop();
+        map.SetTile(gridPosition, null);
+        stones.SetTile(gridPosition, null);
+    }
+}
+
---
+++ Assets/Scripts/PopupManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject popupCard = default;
+    [SerializeField]
+    private Image popupBackground = default;
+    [SerializeField]
+    private Text popupTitle = default;
+    [SerializeField]
+    private Text popupText = default;
+    [SerializeField]
+    private Text popupButton = default;
+    [SerializeField]
+    private Text popupFooter = default;
+
+    Popup popupJsonParse;
+
+    public bool displayingPopup;
+    string json;
+    string dataPath;
+
+    void Start()
+    {
+        dataPath = Application.streamingAssetsPath;
+        json = File.ReadAllText(dataPath + "/Popup.json");
+        popupJsonParse = JsonUtility.FromJson<Popup>(json);
+        popupCard.SetActive(false);
+        displayingPopup = false;
+    }
+
+    public void DisplayPopup()
+    {
+        Color bgColor;
+        popupCard.SetActive(true);
+        displayingPopup = true;
+
+        popupTitle.text = popupJsonParse.title;
+        popupButton.text = popupJsonParse.button_text;
+        
+        if(ColorUtility.TryParseHtmlString(popupJsonParse.bg_color, out bgColor)){
+            popupBackground.color = bgColor;
+        }
+
+        popupText.text = "Fun: " + popupJsonParse.stats.fun + "\n" +
+                        "Strong: " + popupJsonParse.stats.strong + "\n" +
+                        "Evil: " + popupJsonParse.stats.evil ;
+
+        popupFooter.text = popupJsonParse.foot_note;
+    }
+
+    public void ClosePopup()
+    {
+        popupCard.SetActive(false);
+        displayingPopup = false;
+    }
+
+    public void ClickedMainButton()
+    {
+        popupJsonParse.stats.fun += 10;
+        popupJsonParse.stats.strong += 10;
+        popupJsonParse.stats.evil += 10;
+
+        popupText.text = "Fun: " + popupJsonParse.stats.fun + "\n" +
+                        "Strong: " + popupJsonParse.stats.strong + "\n" +
+                        "Evil: " + popupJsonParse.stats.evil;
+
+        json = JsonUtility.ToJson(popupJsonParse);
+        File.WriteAllText(dataPath + "/Popup.json", json);
+    }
+
+}
+
+[Serializable]
+class Popup
+{
+    public string title = default;
+    public string bg_color = default;
+    public string button_text = default;
+    public Stats stats = default;
+    public string foot_note = default;
+}
+
+[Serializable]
+class Stats
+{
+    public int fun = default;
+    public int strong = default;
+    public int evil = default;
+}
